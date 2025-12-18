51dd4c55b662ce1894c912b38edd153143674eda
xonecas <seancaetanomartin@gmail.com>
2025-12-18 18:02:40 +0000

    feat: error component

---
+++ src/components/ErrorNotification.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+import ErrorIcon from "@/src/components/icons/ErrorIcon";
+
+type ErrorNotificationProps = {
+  message: string;
+};
+
+export default function ErrorNotification({ message }: ErrorNotificationProps) {
+  return (
+    <div className="pointer-events-auto mb-2 flex w-fit items-center gap-2 rounded-lg bg-neutral-300/80 px-3 py-1 dark:bg-neutral-800/80">
+      <ErrorIcon className="h-4 w-4 text-red-600 dark:text-red-400" />
+      <span className="text-xs text-neutral-600 dark:text-neutral-400">{message}</span>
+    </div>
+  );
+}
+
+
---
+++ src/components/icons/ErrorIcon.tsx
@@ -0,0 +1,20 @@
+export default function ErrorIcon({ className }: { className?: string }) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      className={className}
+    >
+      <circle cx="12" cy="12" r="10" />
+      <path d="M12 8v4" />
+      <path d="M12 16h.01" />
+    </svg>
+  );
+}
+
+
